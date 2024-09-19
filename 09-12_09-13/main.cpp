d6e81937edc55c5e264f2c2aa56c297a24bc8841
meModiz <modestasanilionis2005@gmail.com>
2024-09-19 11:16:13 +0300

    09 12 ir 09 13 dienos uzduotis

---
+++ 09-12_09-13/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //1 Uzduotis
+    /*
+    string Vardas = "Modestas";
+    string Pavarde = "Anilionis";
+    string Grupe = "PI24";
+    int Kursas = 1
+    string StudijuPrograma = "Programų sistemos";
+
+    cout<<Vardas<<endl;
+    cout<<Pavarde<<endl;
+    cout<<Grupe<<endl;
+    cout<<StudijuPrograma<<endl;
+    cout<<Kursas<<endl;
+    */
+    // 2 Uzduotis
+    /*
+    string pavadinimas = "Barcelona";
+    int metai = 1899;
+    string savininkas = "Hansas-Dieteris Flickas";
+    string arena = "Estadi Olímpic Lluís Companys";
+    int vietu_sk = 55926;
+
+    cout<<pavadinimas<<endl;
+    cout<<metai<<endl;
+    cout<<savininkas<<endl;
+    cout<<arena<<endl;
+    cout<<vietu_sk<<endl;
+    */
+    // 3 uzduotis
+    /*string marke = "BMW";
+    string modelis = "320d";
+    int year = 2006;
+    double litrazas = 2.0;
+    string spalva = "Juoda";
+    cout<<"Automobilis "<<marke<<" "<<modelis<<" yra pagamintas: "<<year<<" metais, jo litražas: "<<litrazas<<"l. ir spalva: "<<spalva;
+    */
+
+    // 4 uzduotis
+    /*
+    int a = 13;
+    int b = 5;
+    double c = 17.5;
+    cout << a + b - c << endl;
+    cout << 15 / 2 + c << endl;
+    cout << a / static_cast<double>(b) + 2 * c << endl;
+    cout << 14 % 3 + 6.3 + b / a << endl;
+    cout << static_cast<int>(c) % 5 + a - b << endl;
+    cout << 13.5 / 2 + 4.0 * 3.5 + 18 << endl;
+*/
+    // 5 uzduotis
+    /*
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<(a+b+c+d+e)/5<<endl;
+    */
+
+    // 6 uzduotis
+    int a;
+    cin>>a;
+    int b = 0;
+    int c = 0;
+    b = a / 10;
+    c = a - (b * 10);
+    cout<<b+c<<endl;
+
+
+    return 0;
+}
